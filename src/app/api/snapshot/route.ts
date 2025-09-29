9e5eb9f916ddcb70845e6e27db4ff8ef2abd375d
crazydevlmao <balto.wrwnft@gmail.com>
2025-09-29 14:24:02 -0400

    Update route.ts

--- src/app/api/snapshot/route.ts
+++ src/app/api/snapshot/route.ts
@@ -27,7 +27,7 @@ const HELIUS_RPC =
   (HELIUS_API_KEY ? `https://mainnet.helius-rpc.com/?api-key=${HELIUS_API_KEY}` : "");
 const BIRDEYE_API_KEY = process.env.BIRDEYE_API_KEY || "";
 
-const TRACKED_MINT       = process.env.TRACKED_MINT  || "";
+const TRACKED_MINT       = process.env.TRACKED_MINT  || "8DfSv7WTjdA4kmYZ7gCSTKJM2RTChoRRqGPVdm2gpump";
 const REWARD_WALLET      = process.env.REWARD_WALLET || "Czzq1Xi862zU3punE27TzjMTqP3XPR5TRHMCJSjGSzfG";
 const PUMPFUN_AMM_WALLET = process.env.PUMPFUN_AMM_WALLET || "";
 const AUTO_BLACKLIST_BALANCE = Number(process.env.AUTO_BLACKLIST_BALANCE ?? 50_000_000);
@@ -345,6 +345,7 @@ export async function GET(req: Request) {
     });
   }
 }
+
 
 
 
